b65176ef027b6ccd0e127095a454e7c5dbac2f70
Jingkai <rock_jk@msn.com>
2019-10-13 00:09:35 +0800

    Create Tavern.kt

---
+++ src/Tavern.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    var beverage = readLine()?.let{
+        if(it.isNotBlank()){
+            it.capitalize()
+        } else {
+            "Buttered Ale"
+        }
+    }
+
+//    beverage = null
+    println(beverage)
+
+}
