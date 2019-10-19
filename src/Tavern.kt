6298bd73cc1c962e751b3bef66aa4c8199630ff3
Jingkai <rock_jk@msn.com>
2019-10-19 22:39:15 +0800

    Modify Tavern.kt

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,14 +1,76 @@
-fun main() {
+import kotlin.math.roundToInt
 
-    var beverage = readLine()?.let{
-        if(it.isNotBlank()){
-            it.capitalize()
-        } else {
-            "Buttered Ale"
+const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
+
+
+fun main() {
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")){
+        when(it.value){
+            "a" -> "4"
+            "A" -> "4"
+            "e" -> "3"
+            "E" -> "3"
+            "i" -> "1"
+            "I" -> "1"
+            "o" -> "0"
+            "O" -> "0"
+            "u" -> "|_|"
+            "U" -> "|_|"
+            else -> it.value
         }
     }
 
-//    beverage = null
-    println(beverage)
 
+fun performancePurchase(price: Double) {
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("Total purse: $totalPurse")
+    println("Purchasing item for $price")
+
+
+    if (totalPurse >= price){
+        val remainingBalance = totalPurse - price
+        println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+
+        val remainingGold = remainingBalance.toInt()
+        val remainingSliver = (remainingBalance % 1 * 100).roundToInt()
+        playerGold = remainingGold
+        playerSilver = remainingSliver
+        displayBalance()
+    } else {
+        println("Not enough money left!")
+    }
+}
+
+private fun displayBalance() {
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
+}
+
+
+
+private fun placeOrder(menuData: String){
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    println("Madrigal buys a $name($type) for $price.")
+
+    performancePurchase(price.toDouble())
+    performancePurchase(price.toDouble())
+    performancePurchase(price.toDouble())
+
+    val phrase = if (name == "Dragon's Breath"){
+        "Madrigal exclaims: ${toDragonSpeak("DRAGON'S BREATH: IT'S GOT WHAT ADVENTURERS CRAVE!")}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
 }
